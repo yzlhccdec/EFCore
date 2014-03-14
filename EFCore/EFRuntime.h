39bd57e7587d092ee288ce94f138ffc5f370e74d
AaronYi <zhuolin.yi@gmail.com>
2014-03-14 15:12:59 +0800

    [bugfix]加入系统版本判断逻辑

--- EFCore/EFRuntime.h
+++ EFCore/EFRuntime.h
@@ -21,6 +21,12 @@
 #   define ULog(...)
 #endif
 
+#define IOS_VERSION_EQUAL_TO(v)                  ([[[UIDevice currentDevice] systemVersion] compare:v options:NSNumericSearch] == NSOrderedSame)
+#define IOS_VERSION_GREATER_THAN(v)              ([[[UIDevice currentDevice] systemVersion] compare:v options:NSNumericSearch] == NSOrderedDescending)
+#define IOS_VERSION_GREATER_THAN_OR_EQUAL_TO(v)  ([[[UIDevice currentDevice] systemVersion] compare:v options:NSNumericSearch] != NSOrderedAscending)
+#define IOS_VERSION_LESS_THAN(v)                ([[[UIDevice currentDevice] systemVersion] compare:v options:NSNumericSearch] == NSOrderedAscending)
+#define IOS_VERSION_LESS_THAN_OR_EQUAL_TO(v)    ([[[UIDevice currentDevice] systemVersion] compare:v options:NSNumericSearch] != NSOrderedDescending)
+
 typedef UIImage* (^ImageDrawingBlock)();
 
 UIImage* cachedImage(NSString *imageName, ImageDrawingBlock block);
