c69118f22b0643b4bf4f78a77134b48decc09c94
yizhuolin <yizhuolin@i-calorie.com>
2015-12-15 13:15:27 +0800

    修复EFSQLiteObject无法处理protocol声明的属性问题

--- EFCore/EFData/EFSQLiteObject.m
+++ EFCore/EFData/EFSQLiteObject.m
@@ -13,6 +13,9 @@
 #import "EFSQLiteObject.h"
 #import "EFKeyValuePair.h"
 #import "EFSQLiteObject+EFPrivate.h"
+#import "EXTRuntimeExtensions.h"
+#import "EXTScope.h"
+
 
 @interface EFSQLiteObject ()
 {
@@ -26,31 +29,20 @@
 
 @end
 
-static NSMutableDictionary *sFieldProperties;
-
 @implementation EFSQLiteObject
-
-+ (void)initialize
-{
-    if ([self class] == [EFSQLiteObject class]) {
-        sFieldProperties = [NSMutableDictionary dictionary];
-    } else {
-        [self __inspectProperties];
-    }
-}
 
 - (id)initWithFMResultSet:(FMResultSet *)resultSet
 {
     return [self initWithFMResultSet:resultSet ignoreFieldNotExist:NO];
 }
 
-- (id)initWithFMResultSet:(FMResultSet *)resultSet ignoreFieldNotExist:(BOOL) ignore
+- (id)initWithFMResultSet:(FMResultSet *)resultSet ignoreFieldNotExist:(BOOL)ignore
 {
     self = [super init];
     if (self) {
         for (EFKeyValuePair *field in [[self class] fieldsForPersistence]) {
-            if(ignore && ![[resultSet columnNameToIndexMap] objectForKey:[field.key lowercaseString]])
-            { 
+            if (ignore && ![[resultSet columnNameToIndexMap] objectForKey:[field.key lowercaseString]]) {
+                 
                 continue; 
             }
             id value = resultSet[field.key];
@@ -75,7 +67,69 @@ static NSMutableDictionary *sFieldProperties;
 
 + (NSArray *)fieldsForPersistence
 {
-    return [sFieldProperties[NSStringFromClass([self class])] allValues];
+    if (self == [EFSQLiteObject class]) {
+        return nil;
+    }
+
+    NSArray *cachedFields = objc_getAssociatedObject(self, _cmd);
+    if (cachedFields != nil) {
+        return cachedFields;
+    }
+
+    NSMutableArray *fields      = [[[self superclass] fieldsForPersistence] mutableCopy];
+
+    if (!fields) {
+        fields = [NSMutableArray array];
+    }
+
+    unsigned int    count;
+    objc_property_t *properties = class_copyPropertyList(self, &count);
+
+    NSSet               *excludedProperties = [self excludedFields];
+    NSMutableDictionary *propertyIndex      = [[NSMutableDictionary alloc] init];
+
+    for (int i = 0; i < count; i++) {
+        EFKeyValuePair *pair = [[EFKeyValuePair alloc] initWithKey:@(property_getName(properties[i])) andValue:@(property_getAttributes(properties[i]))];
+        if ([self __isStorageProperty:pair.key]) {
+            propertyIndex[pair.key] = pair;
+        }
+    }
+
+    [propertyIndex removeObjectsForKeys:excludedProperties.allObjects];
+    free(properties);
+
+    [fields addObjectsFromArray:propertyIndex.allValues];
+    objc_setAssociatedObject(self, _cmd, fields, OBJC_ASSOCIATION_COPY);
+
+    return fields;
+}
+
++ (BOOL)__isStorageProperty:(NSString *)propertyKey
+{
+    objc_property_t property = class_getProperty(self.class, propertyKey.UTF8String);
+
+    if (property == NULL) {
+        return NO;
+    }
+
+    ext_propertyAttributes *attributes = ext_copyPropertyAttributes(property);
+    @onExit{
+        free(attributes);
+    };
+
+    BOOL hasGetter = [self instancesRespondToSelector:attributes->getter];
+    BOOL hasSetter = [self instancesRespondToSelector:attributes->setter];
+    if (!attributes->dynamic && attributes->ivar == NULL && !hasGetter && !hasSetter) {
+        return NO;
+    } else if (attributes->readonly && attributes->ivar == NULL) {
+        if ([self isEqual:EFSQLiteObject.class]) {
+            return NO;
+        } else {
+            return [self.superclass __isStorageProperty:propertyKey];
+        }
+    } else {
+        return YES;
+    }
 }
 
 + (NSArray *)primaryKey
@@ -89,31 +143,6 @@ static NSMutableDictionary *sFieldProperties;
     return NSStringFromClass([self class]);
 }
 
-+ (void)__inspectProperties
-{
-    NSMutableDictionary *propertyIndex = [sFieldProperties[NSStringFromClass([self superclass])] mutableCopy];
-    if (propertyIndex == nil) {
-        propertyIndex = [[NSMutableDictionary alloc] init];
-    }
-
-    Class class                 = [self class];
-
-    unsigned int      count;
-    objc_property_t *properties = class_copyPropertyList(class, &count);
-
-    NSSet *excludedProperties = [self excludedFields];
-
-    for (int i = 0; i < count; i++) {
-        EFKeyValuePair *pair = [[EFKeyValuePair alloc] initWithKey:@(property_getName(properties[i])) andValue:@(property_getAttributes(properties[i]))];
-        propertyIndex[pair.key] = pair;
-    }
-
-    [propertyIndex removeObjectsForKeys:excludedProperties.allObjects];
-    free(properties);
-
-    sFieldProperties[NSStringFromClass([self class])] = propertyIndex;
-}
-
 - (void)startModification
 {
     _changedProperties = [[NSMutableArray alloc] init];
@@ -125,7 +154,7 @@ static NSMutableDictionary *sFieldProperties;
 - (void)endModification
 {
     //delay clean the changedProperties
-   // [_changedProperties removeAllObjects];
+    // [_changedProperties removeAllObjects];
 
     if (_isKVORegistered) {
         [self unregisterFromKVO];
